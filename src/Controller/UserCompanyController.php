69b919b00f9a28e791625f8367f62ba4872fc5cd
arnaud.boubli <webmaster@jetunoo.fr>
2022-08-10 15:33:30 +0200

    ✨ method post userCompany

--- src/Controller/UserCompanyController.php
+++ src/Controller/UserCompanyController.php
@@ -2,14 +2,16 @@
 
 namespace App\Controller;
 
-use App\Entity\Phone;
 use App\Entity\UserCompany;
-use App\Repository\PhoneRepository;
+use App\Repository\CompanyRepository;
 use App\Repository\UserCompanyRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Serializer\SerializerInterface;
 
 class UserCompanyController extends AbstractController
@@ -29,4 +31,39 @@ class UserCompanyController extends AbstractController
         $jsonUserCompany = $serializer->serialize($userCompany, 'json', ['groups' => 'getUserCompany']);
         return new JsonResponse($jsonUserCompany, Response::HTTP_OK, ['accept' => 'json'], true);
     }
+
+    #[Route('/api/usercompanies/{id}', name: 'deleteUserCompanies', methods: ['DELETE'])]
+    public function deleteUserCompanies(UserCompany $userCompany, EntityManagerInterface $em): JsonResponse 
+    {
+        $em->remove($userCompany);
+        $em->flush();
+
+        return new JsonResponse(null, Response::HTTP_NO_CONTENT);
+    }
+
+    #[Route('/api/usercompanies', name:"createUserCompanies", methods: ['POST'])]
+    public function createUserCompanies(Request $request,CompanyRepository $companyRepository, SerializerInterface $serializer, EntityManagerInterface $em, UrlGeneratorInterface $urlGenerator): JsonResponse 
+    {
+
+        $UserCompany = $serializer->deserialize($request->getContent(), UserCompany::class, 'json');
+
+        // Récupération de l'ensemble des données envoyées sous forme de tableau
+        $content = $request->toArray();
+
+        // Récupération de l'idCompany. S'il n'est pas défini, alors on met -1 par défaut.
+        $idCompany = $content['idCompany'] ?? -1;
+
+        // On cherche le userCompany qui correspond et on l'assigne à la company.
+        // Si "find" ne trouve pas le userCompany, alors null sera retourné.
+        $UserCompany->setCompany($companyRepository->find($idCompany));
+
+        $em->persist($UserCompany);
+        $em->flush();
+
+        $jsonUserCompany = $serializer->serialize($UserCompany, 'json', ['groups' => 'getUserCompany']);
+        
+        $location = $urlGenerator->generate('detailUserCompanies', ['id' => $UserCompany->getId()], UrlGeneratorInterface::ABSOLUTE_URL);
+
+        return new JsonResponse($jsonUserCompany, Response::HTTP_CREATED, ["Location" => $location], true);
+   }
 }
